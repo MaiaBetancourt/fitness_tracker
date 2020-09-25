c16500da341ad64b525111883e054c8f8818af48
Maria <maria.betancourt15@yahoo.com>
2020-09-24 17:30:57 -0700

    added models

---
+++ models/workout.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  date: {
+      type: Date,
+      default: Date.now()
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "String is Required",
+      },
+
+      type: {
+        type: String,
+        required: true,
+      },
+
+      duration: Number,
+
+      weight: Number,
+
+      reps: Number,
+
+      sets: Number,
+
+      distance: Number,
+    }
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,69 +1,42 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const db = require("./models");
 
 const app = express();
 const PORT = 8080;
-
-// const animals = [
-//   {
-//     name: "Deku",
-//     age: "2",
-//     cuteness: "10",
-//   },
-// ];
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-
-// GET - /api/animals
-// View all animals
-// app.get("/api/animals", function (req, res) {
-//   res.json(animals);
-// });
-
-// GET - /api/animals/:name
-// Search for a specific animal
-// app.get("/api/animals/:name", function (req, res) {
-//   const { name } = req.params;
-//   for (let i = 0; i < animals.length; i++) {
-//     const animal = animals[i];
-//     if (animal.name.toLowerCase() === name.toLowerCase())
-//       return res.json(animal);
-//   }
-//   return res.status(404).send("Can not find the animal you were looking for!");
-// });
-
-// app.post("/api/signup", (req, res) => {
-//   db.User.create({
-//     email: req.body.email,
-//     password: req.body.password,
-//   })
-//     .then(() => {
-//       res.redirect(307, "/api/login");
-//     })
-//     .catch((err) => {
-//       res.status(401).json(err);
-//     });
-// });
-
-// Add an animal
-// POST - /api/animals
-// name - String - UID
-// age - Number
-// cuteness - Number
-app.post("/api/workouts", function(req, res){
- 
-  res.send("PLEASE WORK");
+app.get("/exercise", function (req, res) {
+  res.sendFile(__dirname + "/public/exercise.html");
 });
+
+app.get("/stats", function (req, res) {
+  res.sendFile(__dirname + "/public/stats.html");
+});
+
+app.post("/api/workouts", function (req, res) {
+  console.log(req.body.name);
+  res.send(`request got sent ${req.body.name}`);
+  // console.log(req.body);
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
