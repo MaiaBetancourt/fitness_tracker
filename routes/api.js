fde4d6d9ae2e57338ebfdecc100a247ad76458f5
Maria <maria.betancourt15@yahoo.com>
2020-09-25 15:50:40 -0700

    api routes re routed

--- routes/api.js
+++ routes/api.js
@@ -2,12 +2,16 @@ const app = require("express").Router();
 const db = require("../models");
 
 app.get("/api/workouts", async function (req, res) {
-    res.json(await db.Workout.find());
+  res.json(await db.Workout.find());
 });
 
-app.post("/api/workouts", function (req, res) {
-    // console.log(req.body.name);
-    // res.send(`request got sent ${req.body.name}`);
-    console.log(req.body);
+app.post("/api/workouts", async function (req, res) {
+  // console.log(req.body.name);
+  // res.send(`request got sent ${req.body.name}`);
+  // console.log(req.body);
+  res.json(await db.Workout.create(req.body));
+});
 
-  });
+app.put("/api/workouts/:id", async function (req, res) {
+  res.json(await db.Workout.findByIdAndUpdate(req.params.id, req.body));
+});
