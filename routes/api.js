460a66a9ebfc840af5a4fcba9d557d2ae1116f20
Maria <maria.betancourt15@yahoo.com>
2020-09-25 21:01:18 -0700

    console logged inputs

--- routes/api.js
+++ routes/api.js
@@ -23,4 +23,9 @@ app.put("/workouts/:id", async function (req, res) {
   );
 });
 
+db.Workout.find().then((dbWorkouts) => {
+  const workout = dbWorkouts[0].toJSON();
+  console.log(workout);
+})
+
 module.exports = app;
