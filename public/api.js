0df6357bd4d6117efbc87c688bec59815f4d4ac3
Maria <maria.betancourt15@yahoo.com>
2020-10-06 09:53:10 -0700

    base url fix

--- public/api.js
+++ public/api.js
@@ -1,10 +1,9 @@
-const BASE_URL = 'http://localhost:8080'
 
 const API = {
   async getLastWorkout() {
     let res;
     try {
-      res = await fetch(BASE_URL + "/api/workouts");
+      res = await fetch("/api/workouts");
       // res = await fetch("/api/workouts");
     } catch (err) {
       console.log(err)
